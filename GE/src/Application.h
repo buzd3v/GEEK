95aa016e9b7f53042e6862684b2d87fadc9e8365
kien.nguyenxuan <kien.nguyenxuan@gameloft.com>
2024-06-12 09:15:34 +0700

    - Commit to change branch =))

--- GE/src/Application.h
+++ GE/src/Application.h
@@ -2,18 +2,18 @@
 
 namespace Geek {
 
-class Application {
- public:
-  
-   /**
+class Application
+{
+public:
+  /**
    * \brief Start the game. Should be called in your main.cpp
    */
   static void Start();
-  
+
   /**
    * \brief Exit the game, can be called anywhere
    */
   static void Exit();
 };
 
-}  // namespace Geek
+} // namespace Geek
