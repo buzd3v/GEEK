e2677a770f645b72381a99edf2271656bf2004cd
kien.nguyenxuan <kien.nguyenxuan@gameloft.com>
2024-06-17 18:28:25 +0700

    Add input config base

--- GE/src/Input/ActionConfig.h
+++ GE/src/Input/ActionConfig.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "Core/IConfigVar.h"
-
+#include "KeyCode.h"
 namespace Geek
 {
 	class ActionConfig : public IConfigVar
 	{
+
+	private:
+		bool m_bIsSingle;
+		
+
 	public:
 		// Inherited via IConfigVar
 		void BindToConfig(pugi::xml_node& root) override;
