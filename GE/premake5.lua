78720e86ef5f93f685dc82e00e93fd9ea91cbbe8
Kien Nguyen <kanashakze02@gmail.com>
2024-06-18 03:21:51 +0700

    NOTHING

--- GE/premake5.lua
+++ GE/premake5.lua
@@ -5,7 +5,8 @@ project "GE"
 	staticruntime "off"
 
 	libdirs { 
-	 	"./External/vld/lib/Win64"
+	 	"./External/vld/lib/Win64",
+		"./External/vld/bin/Win64"
 	}
 
 	links {
@@ -64,9 +65,12 @@ project "GE"
 	
 function copyVldNeeded()
     return {
-        "{COPY} External/vld/bin/Win64/** %{cfg.targetdir}/bin/Debug/",
+        "{COPY} External/vld/bin/Win64/** bin/%{cfg.platform}/%{cfg.buildcfg}",
+		"{COPY} External/vld/vld.ini bin/%{cfg.platform}/%{cfg.buildcfg}",
+
     }
 end	
+
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
---
+++ GE/src/Input/ActionManager.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Core/ConfigMgr.h"
+namespace Geek
+{
+	class ActionConfig;
+	class ActionManager : ConfigMgr <ActionConfig>
+	{
+	public:
+
+	};
+}
